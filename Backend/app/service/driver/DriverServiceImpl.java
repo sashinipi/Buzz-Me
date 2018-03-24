d733b77c2cffaa0834622e32a8e04849a8ddb9b0
sashinipi <sashini.2014509@iit.ac.lk>
2018-03-24 16:54:01 +0530

    Format

---
+++ Backend/app/service/driver/DriverServiceImpl.java
@@ -0,0 +1,59 @@
+package service.driver;
+
+import dto.DriverDTO;
+
+/**
+ * Created by Sashinip on 3/24/2018.
+ */
+
+/**
+ * Implementation of driver service.
+ *
+ */
+
+public class DriverServiceImpl implements DriverService {
+
+
+
+    /**
+     * Add a new driver.
+     *
+     * @param driverDTO DTO containing passenger information
+     * @return             Success or fail status
+     */
+
+    @Override
+    public String addNewDriver(DriverDTO driverDTO) {
+        return null;
+    }
+
+    @Override
+    public String editDriver(DriverDTO driverDTO) {
+        return null;
+    }
+
+    @Override
+    public String deleteDriver(DriverDTO driverDTO) {
+        return null;
+    }
+
+    @Override
+    public String getBusbyId(DriverDTO driverDTO) {
+        return null;
+    }
+
+    @Override
+    public String loginDriver(DriverDTO driverDTO) {
+        return null;
+    }
+
+    @Override
+    public String startRideDriver(DriverDTO driverDTO) {
+        return null;
+    }
+
+    @Override
+    public String updateSpeedOfTheBus(DriverDTO driverDTO) {
+        return null;
+    }
+}
--- Backend/project/plugins.sbt
+++ Backend/project/plugins.sbt
@@ -3,3 +3,4 @@ logLevel := Level.Warn
 resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
 
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.9")
+addSbtPlugin("com.typesafe.sbt" % "sbt-play-ebean" % "4.1.0")
