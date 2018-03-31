ede61da96ff5b90cd8d3e32f08bd25bd3393d7cb
sashinipi <sashini.2014509@iit.ac.lk>
2018-03-31 13:10:28 +0530

    DriverDao Changes

--- Backend/app/dao/driver/DriverDao.java
+++ Backend/app/dao/driver/DriverDao.java
@@ -8,12 +8,34 @@ import models.DriverModel;
 
 public interface DriverDao {
 
+    /**
+     * Add new Driver.
+     * @param driverModel
+     * @return driver ID
+     * @throws Exception
+     */
     int addDriver(DriverModel driverModel) throws Exception;
 
+    /**
+     * Edit Driver.
+     * @param driverModel
+     * @throws Exception
+     */
     void editDriver(DriverModel driverModel) throws Exception;
 
+    /**
+     * Delete Driver
+     * @param driverModel
+     * @throws Exception
+     */
     void deleteDriver(DriverModel driverModel) throws Exception;
 
+    /**
+     * Get Driver Details
+     * @param passengerID
+     * @return DriverModel
+     * @throws Exception
+     */
     DriverModel getDriverByID(int passengerID) throws Exception;
 
 }
